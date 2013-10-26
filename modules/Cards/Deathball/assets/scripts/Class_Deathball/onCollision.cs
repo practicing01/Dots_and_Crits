c40996737691c203c967039b0ee7be632609df6a
practicing01 <globalobject01@hotmail.com>
2013-10-26 11:06:37 -0400

    Nano bug fix.

--- modules/Cards/Deathball/assets/scripts/Class_Deathball/onCollision.cs
+++ modules/Cards/Deathball/assets/scripts/Class_Deathball/onCollision.cs
@@ -3,13 +3,13 @@ function Class_Deathball::onCollision(%this,%Colliding_Object,%Collision_Details
 
 if (%Colliding_Object.class$="Class_Composite_Sprite_Player")
 {
-echo("Collided with player");
+
 %Colliding_Object.Module_ID_Parent.Action_Update_Health(%Colliding_Object.Script_Object_Parent,-10);
 
 }
 else
 {
-echo("Collided with object");
+
 %Colliding_Object.Update_Health(-10);
 
 }
--- modules/Player_Sprites/Ayn/assets/scripts/Actions/Action_Update_Health.cs
+++ modules/Player_Sprites/Ayn/assets/scripts/Actions/Action_Update_Health.cs
@@ -1,7 +1,7 @@
 function Module_Player_Sprite_Ayn::Action_Update_Health(%this,%Player_Information,%Health)
 {
 
-%Player_Information.Health+=%Health;echo(%Player_Information.Health);
+%Player_Information.Health+=%Health;
 
 $Module_ID_Map_Loaded.Player_Health(%Player_Information);
 
